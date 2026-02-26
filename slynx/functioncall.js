d7b911800a62e0761b0a5f6d8d6f2d753c592d3a
cykna <kina.lima1302@proton.me>
2026-02-26 12:08:15 -0300

    fix: fixed error on type checker where Add and Sub operators would result boolean, and && and || wouldnt result in boolean

--- slynx/functioncall.js
+++ slynx/functioncall.js
@@ -9,6 +9,11 @@ function c1() {
 }
 function c2() {
   let v0;
-  v0 = c0("Jorge", 44);
+  v0 = {
+    f0: "maria",
+    f1: 22
+  };
+  let v1;
+  v1 = c0("Jorge", 44 + v0.f18446744073709551615);
   return c1();
 }
